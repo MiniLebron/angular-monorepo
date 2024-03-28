ee38cf6e06b3a01ddcfcb2309f47bc95161031a2
shimnx <shimnx@schaeffler.com>
2024-03-28 13:38:34 +0800

    add basic components

--- apps/inventory/src/app/app.routes.ts
+++ apps/inventory/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path:'/shared-ui',
+        loadComponent: () =>
+        import('@angular-monorepo/schare-ui').then((m) => m.SchareUiComponent),
+    }
+];
